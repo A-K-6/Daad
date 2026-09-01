d0d3c3ec7bfff530233de5f923377a8722df1186
Aeen <aeenkarimian@gmail.com>
2026-09-01 23:39:37 +0330

    feat: add mobile phone HTTPS testing support

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    process.env.HTTPS === 'true' ? basicSsl() : undefined,
+  ].filter(Boolean),
   base: './',
   clearScreen: false,
   server: {
     port: 1420,
     strictPort: true,
-    host: true, // Exposes dev server to local network (for mobile phone testing)
+    host: true, // Exposes dev server to local network for mobile phone testing
   },
   envPrefix: ['VITE_', 'TAURI_'],
   build: {
